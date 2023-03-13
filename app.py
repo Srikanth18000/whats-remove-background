dd3a5a693d176bf4e20025d383b85dc53e622107
Srikanth18000 <95920135+Srikanth18000@users.noreply.github.com>
2023-03-13 21:36:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify, make_response
-import requests
 from twilio.twiml.messaging_response import MessagingResponse
+import requests
+import os
 
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
@@ -9,31 +10,39 @@ app = Flask(__name__)
 endpoint = 'https://api.remove.bg/v1.0/removebg'
 api_key = 'ekz5gjB1vnd6REFibrBbnoMD'
 
-# Route to Twilio services
-@app.route('/sms', methods=['POST'])
-def sms():
-    # Get the incoming message from Twilio
-    message = request.form['Body']
-    
-    # Remove the background from the image using remove.bg API
-    response = requests.post(
-        endpoint,
-        headers={'X-Api-Key': api_key},
-        files={'image_url': message},
-        data={'size': 'auto'}
-    )
-    
-    # Send the image to the user using Twilio
-    error_message = {"errors":[{"title":"No image given","code":"missing_source","detail":"Please provide the source image in the image_url, image_file or image_file_b64 parameter."}]}
-    response = make_response(jsonify(error_message), 400)
-    response.headers['Content-Type'] = 'application/json'
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    # Get the incoming message
+    incoming_msg = request.values.get('Body', '').strip()
+    # Get the media URL from the message
+    media_url = request.values.get('MediaUrl0', '')
+    # Check if the message has media
+    if media_url:
+        # Download the image
+        response = requests.get(media_url)
+        img = response.content
+        # Set up the API headers and parameters
+        headers = {'X-Api-Key': api_key}
+        files = {'image_file': img}
+        params = {'size': 'auto'}
+        # Send a POST request to Remove.bg API to remove the background
+        response = requests.post(endpoint, headers=headers, files=files, params=params)
+        # Save the resulting image
+        with open('result.png', 'wb') as out:
+            out.write(response.content)
+        # Send the resulting image back to the user
+        resp = MessagingResponse()
+        message = resp.message()
+        message.media('http://{}/result.png'.format(request.host))
+        return str(resp)
+    else:
+        # Send a default message if no media is found
+        resp = MessagingResponse()
+        resp.message("Send me an image with a background to remove it!")
+        return str(resp)
 
-    resp = MessagingResponse()
-    resp.body('Error: ' + response.get_data().decode('utf-8'))
-    resp.content_type = 'application/json'
-    return str(resp)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+
 
