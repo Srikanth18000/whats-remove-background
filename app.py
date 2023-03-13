42d70fee058eacbe5d66bf0d6b6932c39b71f077
Srikanth18000 <95920135+Srikanth18000@users.noreply.github.com>
2023-03-13 20:03:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,13 @@ def sms():
     )
     
     # Send the image to the user using Twilio
-    resp = MessagingResponse()
-    resp.message().media(response.content)
+    error_message = {"errors":[{"title":"No image given","code":"missing_source","detail":"Please provide the source image in the image_url, image_file or image_file_b64 parameter."}]}
+    response = make_response(jsonify(error_message), 400)
+    response.headers['Content-Type'] = 'application/json'
+
+    resp = messaging_response.Message()
+    resp.body('Error: ' + response.get_data().decode('utf-8'))
+    resp.content_type = 'application/json'
     return str(resp)
 
 
